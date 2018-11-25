fd938b7d58d1d846bf7bf2e7ee2820db9da7ee4c
daiyujun <43599550+daiyujun@users.noreply.github.com>
2018-11-26 01:04:18 +1100

    Add files via upload

---
+++ SufPos/graph.h
@@ -0,0 +1,33 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "gadget.h"
+#include <vector>
+#include <string>
+
+class Graph
+{
+    public:
+        Graph();
+        Graph(char* fname);
+        ~Graph();
+
+        void build(char *fname);
+        void print();
+        int dest(int v, int k);
+        int deg(int v);
+        int V();
+        int E();
+        int hindex();
+        VI& adjvec(int i) {return gvec[i];}
+        bool hasCore(int k);
+        VVI gvec;
+    
+    private:
+    
+        static const int maxe = 200000000;
+        int n, m;
+		int *degree, *start, *elist;
+};
+
+#endif // GRAPH_H
